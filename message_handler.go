3283fc2b48b1b016714db6c1e5708c7dd65947dd
Coyot Willy <coyotwilly@gmail.com>
2024-12-18 21:38:42 +0100

    Fix exception handling
    Fixes bug were during sudden shutdown client connection were not closed properly by the app.

--- message_handler.go
+++ message_handler.go
@@ -1,5 +1,7 @@
 package main
 
+import "log"
+
 func (client *Client) Send(message string) error {
 	return client.SendBytes([]byte(message))
 }
@@ -8,7 +10,12 @@ func (client *Client) SendBytes(message []byte) error {
 	_, err := client.connection.Write(message)
 
 	if err != nil {
-		client.connection.Close()
+		err := client.connection.Close()
+		if err != nil {
+			log.Println("Failed to close connection")
+
+			return err
+		}
 		client.Server.onClientDisconnect(client, err)
 	}
 
